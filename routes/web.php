91b1cbc8722ed940cc6dd49562a9adc1af1cd4ab
yaden <brildent@gmail.com>
2022-06-25 14:16:45 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    var_dump("didieu");
+    $storagePath  = Storage::disk('local');
+    var_dump( $storagePath );
+
     return view('welcome');
 });
 
