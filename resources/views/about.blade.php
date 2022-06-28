03f5a00e45511f388ac09e8caaf9a9e7c6cd58d5
yadenmustopa <brildent@gmail.com>
2022-06-28 09:52:05 +0700

    add section and section title

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,6 @@
-@foreach ( $arr as $item )
-    @if( $loop->first)
-        {{-- @{{ $item["alamat"] }} --}}
-    @endif
-        
-@endforeach
-<h1>This is about page</h1>
+@extends("layouts.app")
+@section("title","about Us")
+
+@section('content')
+    <h1>About Us</h1>
+@endsection
