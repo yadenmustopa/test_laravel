d58ab53eb5bc73fabba5e1c5a6b0fcbcc8e26bcb
yadenmustopa <brildent@gmail.com>
2022-06-28 10:00:09 +0700

    add navigation

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,6 @@
+<nav class="flex justify-center space-x-4">
+    <a href="/" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
+    <a href="/about" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">About</a>
+    <a href="/contact" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Contact</a>
+    {{-- <a href="/" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Reports</a> --}}
+</nav>
